4377dc8c0be39aa2788b0107687ec7886d82c1e9
Holger Stitz <holger.stitz@datavisyn.io>
2021-02-18 09:27:54 +0100

    Add screenshot script

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# clue-session-screenshot
-Takes screenshots for every state from exported CLUE sessions (aka provenance graphs)
+# CLUE Session Screenshot
+
+This little script takes some exported CLUE sessions (aka provenance graphs) and imports them programmatically
+and takes a screenshot of every step/state in the session.
+
+The tool iterates over all JSON files found in the *sessions* directory.
+The captured screenshots are then stored in the *screenshots* directory.
+
+The script uses puppeteer to launch a headless Chromium browser and navigates to the app. Currently, the script is
+customized to the Vega Gapminder app, but can be easily adapted for other CLUE applications.
+
+## Prerequisite
+
+* Node.js >= v12
+
+## Setup and run
+
+1. `git clone https://github.com/thinkh/clue-session-screenshot`
+2. `npm install` -> can take a few minutes as it downloads the headless Chromium (~120 MB)
+3. Copy all session JSON files into *session* directory
+4. `npm start`
+5. Find the captured screenshots for each session in the *screenshots* directory
+
+## Settings
+
+Change the URL to the app and the pathes to the local directories can be changed in the *index.js* file.
+
+## Known issues
+
+Chromium does not work in *Windows Subsystem for Linux* (WSL) due to missing graphical output.
+Please setup this repository directly on a native Windows or Linux.
---
+++ index.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+// const BASE_URL = 'http://localhost:8080/';
+const BASE_URL = 'https://vega-gapminder.caleydoapp.org/';
+const SESSION_PATH = './sessions';
+const SCREENSHOT_PATH = './screenshots';
+
+function start(baseURL, sessionPath, screenshotPath) {
+    async function captureScreenshotFromSession(sessionFile) {
+        console.log('launch browser');
+        const browser = await puppeteer.launch();
+
+        console.log('open new page');
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        });
+
+        await page.goto(baseURL, {
+            waitUntil: 'networkidle2',
+        });
+        console.log('initial url', page.url());
+
+        // accept cookies to hide cookie bar
+        console.log('accept cookies to hide cookie bar');
+        await page.waitForSelector('#cookie-bar-button');
+        await (await page.$('#cookie-bar-button')).click();
+        await page.waitForSelector('#cookie-bar-button', { hidden: true }); // wait until hide animation finishes
+
+        console.log('open session dropdown');
+        await (await page.$('body > div.box > nav > div.collapse.navbar-collapse > ul:nth-child(4) > li > a')).click();
+
+        console.log('import session locally');
+        await (await page.$('#provenancegraph_import')).click();
+
+        await page.waitForSelector('input[type=file]'); // wait for upload modal dialog to open
+
+        const inputUploadHandle = await page.$('input[type=file]');
+        console.log('select the session file to upload');
+        await Promise.all([
+            inputUploadHandle.uploadFile(sessionFile),
+            page.waitForNavigation({
+                waitUntil: 'networkidle2', // wait again until page has loaded
+            })
+        ]);
+        const sessionUrl = page.url();
+        console.log('new session url', sessionUrl);
+
+        await page.waitForSelector('main'); // wait for the selector which indicates the page initialization has finished
+        await page.waitForSelector('svg.marks'); // wait until vega svg has loaded
+
+        const sessionPath = path.join(screenshotPath, path.basename(sessionFile));
+
+        // create directory for this session
+        if (!fs.existsSync(sessionPath)) {
+            console.log('create new directory for session screenshots', sessionPath);
+            fs.mkdirSync(sessionPath);
+        }
+
+        function extractStateIdsFromSession(sessionFile) {
+            const session = JSON.parse(fs.readFileSync(sessionFile, 'utf8'));
+            const states = session.nodes.filter((node) => node.type === 'state');
+            return states.map((state) => state.id);
+        }
+
+        console.log('extract state ids from session');
+        const stateIds = extractStateIdsFromSession(sessionFile);
+        console.log('number of states', stateIds.length);
+
+        for (let i = 0; i < stateIds.length; i++) {
+            const sessionStateURL = `${sessionUrl}&clue_state=${stateIds[i]}`;
+            const sessionScreenshotPath = path.join(sessionPath, `${stateIds[i]}.png`);
+
+            await page.goto(sessionStateURL, {
+                // waitUntil: 'networkidle2', // no need to wait again since everything is already loaded
+            });
+
+            console.log(`take screenshot of`, sessionStateURL);
+            const mainElement = await page.$('main'); // select main element
+            await mainElement.screenshot({ path: sessionScreenshotPath }); // take screenshot element in puppeteer
+            console.log(`screenshot saved to ${sessionScreenshotPath}`);
+        }
+
+        console.log('close browser');
+        await browser.close();
+    }
+
+    fs.readdir(sessionPath, async (err, files) => {
+        if (err) {
+            throw err;
+        }
+
+        for (let i = 0; i < files.length; i++) {
+            console.log(`start session ${files[i]}`);
+            await captureScreenshotFromSession(path.join(SESSION_PATH, files[i]));
+            console.log(`finished session ${files[i]}`);
+            console.log('----------------------------------------------');
+        }
+        console.log('All done!');
+    });
+}
+
+start(BASE_URL, SESSION_PATH, SCREENSHOT_PATH);
